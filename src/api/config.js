2bf6deadb0a1ef1f16a3803ceb2d11cc85ee3ca0
rhauddy <rhauddyventura@gmail.com>
2017-06-14 19:30:41 -0400

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+  appName: 'xmusic',
+  apiKey: 'd10b23aa4bde3c47ce76f63de5637fd4',
+  secret: 'de0229ebc2b0340ffa7334d7e4e63743',
+  registeredTo: 'rhaygon'
+}
+
+// Application name	xmusic
+// API key	d10b23aa4bde3c47ce76f63de5637fd4
+// Shared secret	de0229ebc2b0340ffa7334d7e4e63743
+// Registered to	rhaygon
